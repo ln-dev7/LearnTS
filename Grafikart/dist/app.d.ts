40a5f1fec9d94192fd587d9d45bc22ba6f280a59
LN <LN@MacBook-Pro-de-Mboa.local>
2022-08-26 14:30:51 +0100

    continue

---
+++ Grafikart/dist/app.d.ts
@@ -0,0 +1,5 @@
+export declare class PointTwo {
+    x: number;
+    y: number;
+    move(dx: number, dy: number): void;
+}
--- dist/app.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class PointTwo {
-    x: number;
-    y: number;
-    move(dx: number, dy: number): void;
-}
